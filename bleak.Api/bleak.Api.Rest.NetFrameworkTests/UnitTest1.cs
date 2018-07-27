f721efc30bd7e342ebe101f3d8d3aa138d0369af
Jamal Khan <jamal.h.khan@gmail.com>
2018-07-26 23:24:27 -0400

    Adding a bleak.Api.Rest.Portable version to ensure compatibility with Android. Hopefully this can be removed before too much longer...

---
+++ bleak.Api/bleak.Api.Rest.NetFrameworkTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using bleak.Api.Rest.Portable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bleak.Api.Rest.NetFrameworkTests
+{
+    [TestClass]
+    public class RestManagerTests
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            var s = "https://google.com";
+            var results = RestManager.ExecuteRestMethod<string, string>(uri: new Uri(s), verb: HttpVerbs.GET, userAgent: "blah blah blah");
+            Assert.IsTrue(results.Results.Contains("<body"));
+        }
+    }
+}
