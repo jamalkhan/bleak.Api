1125c38ddb8d8c827b78483acbd3fc7221f62aba
Jamal Khan <jamal.h.khan@gmail.com>
2018-12-27 17:25:34 -0500

    trying to reoslve build errors

--- bleak.Api.Rest.Common.Tests/NetCore20CoreRestManagerTests.cs
+++ bleak.Api.Rest.Common.Tests/NetCore20CoreRestManagerTests.cs
@@ -6,15 +6,16 @@ namespace bleak.Api.Rest.Core.Tests
     [TestClass]
     public class NetCore20CoreRestManagerTests
     {
-        //[TestMethod]
-        //public void TestLoadWebpage()
-        //{
+        [TestMethod]
+        public void TestPlaceholder()
+        {
+            Assert.IsTrue(true);
         //    var serializer = new JsonSerializer();
         //    IRestManager restManager = new CoreRestManager(
         //        serializer: serializer,
         //        deserializer: serializer);
         //    var results = restManager.ExecuteRestMethod<string, string>(new Uri("http://google.com"));
         //    Assert.IsTrue(results.Results.Contains("<body"));
-        //}
+        }
     }
 }
