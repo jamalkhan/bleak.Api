b56cc5252bcd872f6e6cfb1d887ac7169e7a52c3
Jamal Khan <jamal.h.khan@gmail.com>
2020-09-10 21:52:31 -0400

    Adding Headers to ResponseRequestSummary

--- bleak.Api.Rest.Common/BaseRequestResponseSummary.cs
+++ bleak.Api.Rest.Common/BaseRequestResponseSummary.cs
@@ -7,6 +7,7 @@ namespace bleak.Api.Rest.Common
     {
         public string SerializedRequest { get; set; }
         public string SerializedResponse { get; set; }
+        public IEnumerable<Header> RequestHeaders { get; set; }
         public IEnumerable<Header> ResponseHeaders { get; set; }
         public HttpStatusCode Status { get; set; }
     }
