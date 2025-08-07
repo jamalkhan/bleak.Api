1bc5b427b334676e54114f2f9a7952cc3e52f772
Jamal Khan <jamal.h.khan@gmail.com>
2025-08-07 10:13:22 -0400

    really beginning to hash out RestClient.

---
+++ bleak.Api.Rest.Tests/RestClientTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bleak.Api.Rest;
+using System;
+using System.Threading.Tasks;
+
+namespace bleak.Api.Rest.Tests;
+
+[TestClass]
+public class RestClientTestsTests
+{
+    
+}
